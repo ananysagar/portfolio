832be25997964edc6059ca85fa1b5911a6058272
Sagar <anany.sagar@accenture.com>
2024-04-28 17:49:02 +0530

    Creating the sidebar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,49 @@
-import React from 'react'
-import './navbar.css'
+import React, { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdCloseCircle } from "react-icons/io";
+import "./navbar.css";
 const Navbar = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+    //console.log("Menu is clicked");
+  };
+  
   return (
-    <div className='navbar'>
+    <>
+      <div className="navbar">
         <div className="left-navbar">
-            <div className="logo">A Sagar</div>
-            <span>.</span>
+          <div className="logo">A Sagar</div>
+          <span>.</span>
         </div>
         <div className="right-navbar">
-            <ul>
-                <li>About Me</li>
-                <li>Experience</li>
-                <li>Projects</li>
-                <li>Blogs</li>
-                <li>Contacts</li>
-            </ul>
+          <ul>
+            <li>About Me</li>
+            <li>Experience</li>
+            <li>Projects</li>
+            <li>Blogs</li>
+            <li>Contacts</li>
+          </ul>
         </div>
-    </div>
-  )
-}
+        <div className="hamburger-icons" onClick={toggleSidebar}>
+          {showSidebar ? (
+            <IoMdCloseCircle className="close-icon"/>
+          ) : (
+            <RxHamburgerMenu className="hamburger-menu"/>
+          )}  
+        </div>
+      </div>
+      <div className={`sidebar ${showSidebar ? 'show' : ''}`}>
+        <div className="close-btn" onClick={toggleSidebar}>
+          <IoMdCloseCircle className="close-icon"/>
+        </div>
+        <div className="sidebar-menu">
+          Sidebar Contents
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
