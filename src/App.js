db8675bc9f6f4141138d7e39c63247d454b7f3eb
ananysagar <sagaranany577@gmail.com>
2024-05-01 14:44:31 +0530

    creating the hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <section id="home">Home</section>
+      <section id="home"><Home /></section>
       <section id="aboutme">About Me</section>
       <section id="experience">Experiences</section>
       <section id="project">Projects</section>
