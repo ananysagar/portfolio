2f326b650abe3c7987a46fb1e62dfe143cf717e6
ananysagar <sagaranany577@gmail.com>
2024-05-03 23:53:43 +0530

    format fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,31 +5,42 @@ import "./home.css";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import image from '../images/image.jpg'
+import image from "../images/image.jpg";
 const Home = () => {
   return (
     <div className="home">
       <div className="socials">
-            <a href="www.github.com/ananysagar" className="icons github-icon"><FaGithub /></a>
-            <a href="www.linkedin.com/in/ananysagar" className="icons linkedin-icon"><FaLinkedin /></a>
-            <a href="www.x.com/ananysagar" className="icons twitter-icon"><FaXTwitter /></a>
-
+        <a href="www.github.com/ananysagar" className="icons github-icon">
+          <FaGithub />
+        </a>
+        <a
+          href="www.linkedin.com/in/ananysagar"
+          className="icons linkedin-icon"
+        >
+          <FaLinkedin />
+        </a>
+        <a href="www.x.com/ananysagar" className="icons twitter-icon">
+          <FaXTwitter />
+        </a>
       </div>
       <div className="short-intro">
         <h1>Anany Sagar</h1>
         <div className="designation">
-            <span className="horizontal-line"></span>
-            <h4>Frontend Developer</h4>
-            <span className="horizontal-line"></span>
+          <span className="horizontal-line"></span>
+          <h4>Frontend Developer</h4>
+          <span className="horizontal-line"></span>
         </div>
-        <p>I am a frontend developer based in Bangalore and I am very mush passionate about my work.
-        I am a frontend developer based in Bangalore and I am very mush passionate about my work.
-        I am a frontend developer based in Bangalore and I am very mush passionate about my work.
-        I am a frontend developer based in Bangalore and I am very mush passionate about my work.
+        <p>
+          I am a frontend developer based in Bangalore and I am very mush
+          passionate about my work. I am a frontend developer based in Bangalore
+          and I am very mush passionate about my work. I am a frontend developer
+          based in Bangalore and I am very mush passionate about my work. I am a
+          frontend developer based in Bangalore and I am very mush passionate
+          about my work.
         </p>
       </div>
       <div className="my-pic">
-        <img src={image} alt="" width='350px'/>
+        <img src={image} alt="" width="350px" />
       </div>
     </div>
   );
