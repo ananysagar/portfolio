080f9cdeb7b1b1596c84fb04c95aa5fd2650df60
ananysagar <sagaranany577@gmail.com>
2024-05-05 13:58:39 +0530

    creatinvg the project page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Aboutme from './components/Aboutme';
 import Skills from './components/Skills';
+import Project from './components/Project';
 function App() {
   return (
     <div className="App">
@@ -10,7 +11,7 @@ function App() {
       <section id="home"><Home /></section>
       <section id="aboutme"><Aboutme /></section>
       <section id="skills"><Skills /></section>
-      <section id="project">Projects</section>
+      <section id="project"><Project /></section>
       <section id="blogs">Blogs</section>
       <section id="contacts">Contact</section>
     </div>
