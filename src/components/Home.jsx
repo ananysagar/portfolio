6be1713bf974805e508ca9cab239c686d2f23de0
ananysagar <sagaranany577@gmail.com>
2024-05-12 20:30:06 +0530

    making hero section responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,39 +8,50 @@ import { FaXTwitter } from "react-icons/fa6";
 import image from "../images/image.jpg";
 const Home = () => {
   return (
-    <div className="home">
+    <div className="home mobile-view">
       <div className="socials item">
-        <Link to="https://github.com/ananysagar" target="_blank" className="icons github-icon">
+        <Link
+          to="https://github.com/ananysagar"
+          target="_blank"
+          className="icons github-icon"
+        >
           <FaGithub />
         </Link>
         <Link
-          to="https://linkedin.com/in/ananysagar" target="_blank"
+          to="https://linkedin.com/in/ananysagar"
+          target="_blank"
           className="icons linkedin-icon"
         >
           <FaLinkedin />
         </Link>
-        <Link to="https://x.com/ananysagar" target="_blank" className="icons twitter-icon">
+        <Link
+          to="https://x.com/ananysagar"
+          target="_blank"
+          className="icons twitter-icon"
+        >
           <FaXTwitter />
         </Link>
       </div>
-      <div className="short-intro item">
-        <h1>Anany Sagar</h1>
-        <div className="designation">
-          <span className="horizontal-line"></span>
-          <h4>Frontend Developer</h4>
-          <span className="horizontal-line"></span>
+      <div className="hero-section">
+        <div className="short-intro item">
+          <h1>Anany Sagar</h1>
+          <div className="designation">
+            <span className="horizontal-line"></span>
+            <h4>Frontend Developer</h4>
+            <span className="horizontal-line"></span>
+          </div>
+          <p>
+            I am a frontend developer based in Bangalore and I am very mush
+            passionate about my work. I am a frontend developer based in
+            Bangalore and I am very mush passionate about my work. I am a
+            frontend developer based in Bangalore and I am very mush passionate
+            about my work. I am a frontend developer based in Bangalore and I am
+            very mush passionate about my work.
+          </p>
         </div>
-        <p>
-          I am a frontend developer based in Bangalore and I am very mush
-          passionate about my work. I am a frontend developer based in Bangalore
-          and I am very mush passionate about my work. I am a frontend developer
-          based in Bangalore and I am very mush passionate about my work. I am a
-          frontend developer based in Bangalore and I am very mush passionate
-          about my work.
-        </p>
-      </div>
-      <div className="my-pic item">
-        <img src={image} alt="" width="350px" />
+        <div className="my-pic item">
+          <img src={image} alt="" width="350px" />
+        </div>
       </div>
     </div>
   );
